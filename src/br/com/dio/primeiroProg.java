5240905d2ba61d83d60a9a315b139609af1ecee7
BKKajita <bruno.kajita@tutanota.com>
2021-09-13 21:00:03 -0300

    Cria class livro

--- src/br/com/dio/primeiroProg.java
+++ src/br/com/dio/primeiroProg.java
@@ -5,10 +5,46 @@ import br.com.dio.model.Gato;
 public class primeiroProg {
     public static void main(String[] args) {
         Gato gato = new Gato();
-
         System.out.println(gato);
+
+        Livro livro = new Livro("Os três porquinhos", "300");
+        System.out.println(livro);
 /*        int a = 5;
         int b = 3;
         System.out.println("Hello World!" + " " + (a + b));*/
+    }
+}
+
+class Livro {
+    private String nome;
+    private String numPaginas;
+
+    public Livro(String nome, String numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(String numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas='" + numPaginas + '\'' +
+                '}';
     }
 }
